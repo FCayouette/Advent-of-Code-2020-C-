e0a86a8c667611e3b8daea33a39fa0e314f2f27d
Francois Cayouette <45644908+FCayouette@users.noreply.github.com>
2020-12-01 21:57:35 -0500

    Day 01 F# solution

---
+++ Day01f.fs
@@ -0,0 +1,19 @@
+﻿open System.IO
+
+let Complement value data total =
+    let comp = total - value
+    let result = Array.tryFind((=) comp) data
+    match result with 
+    | Some x -> value * comp
+    | None -> 0
+
+let Part2 value data = 
+    let comp = 2020 - value
+    data |> Array.map(fun x -> Complement x data comp) |> Array.max
+
+[<EntryPoint>]
+let main argv =
+    let data = File.ReadLines(argv.[0]) |> Seq.map System.Int32.Parse |> Seq.toArray
+    data |> Array.map(fun x -> Complement x data 2020) |> Array.max |> printfn "Part 1: %A"
+    data |> Array.map(fun x -> (Part2 x data) * x) |> Array.max |> printfn "Part 2: %A"
+    0 // Returns exit code
