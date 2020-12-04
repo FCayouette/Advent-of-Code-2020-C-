fd54f87c0be293567853bc52f4e02c31c2ad6ddf
Francois Cayouette <45644908+FCayouette@users.noreply.github.com>
2020-12-03 21:10:21 -0500

    Day 3: F# solution and better C++ OO design

--- Day03.cpp
+++ Day03.cpp
@@ -1,48 +1,48 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <array>
+#include <numeric>
+#include <functional>
+
+struct Accumulator
+{
+    constexpr Accumulator(int X, int Y=1) : dx(X), dy(Y) {}
+    void check(const std::string& line)
+    {
+        if (!y && line[x] == '#')
+            ++total;
+        if (++y == dy)
+        {
+            x = (x + dx) % line.size();
+            y = 0;
+        }
+    }
+    constexpr operator long long() const { return total; }
+private:
+    int x = 0, y = 0, dx, dy;
+    long long total = 0;
+};
 
 int main(int argc, char* argv[])
 {
     if (argc < 2)
     {
-        std::cout << "Usage: Day02.exe Datafilename" << std::endl;
+        std::cout << "Usage: Day03.exe Datafilename" << std::endl;
         return -1;
     }
 
     std::ifstream in(argv[1], std::ios::in);
     if (!in) return -1;
 
-    int r1 = 0, r3 = 0, r5 = 0, r7 = 0, r05 = 0, y = 0;
-    long long r1d1 = 0, r3d1 = 0, r5d1 = 0, r7d1 = 0, r1d2 = 0;
+    std::array<Accumulator, 5> ratios = { 3, 1, 5, 7, Accumulator(1, 2) };
     std::string row;
-
     while (in >> row)
-    {
-        static int maxX = row.size();
-    
-        if (row[r1] == '#')
-            ++r1d1;
-        if (row[r3] == '#')
-            ++r3d1;
-        if (row[r5] == '#')
-            ++r5d1;
-        if (row[r7] == '#')
-            ++r7d1;
-        if (!y && row[r05] == '#')
-            ++r1d2;
-
-        r1 = (r1 + 1) % maxX;
-        r3 = (r3 + 3) % maxX;
-        r5 = (r5 + 5) % maxX;
-        r7 = (r7 + 7) % maxX;
-        if (y++)
-        {
-            r05 = (r05 + 1) % maxX;
-            y = 0;
-        }
-    }
-    std::cout << "Part 1: " << r3d1 << std::endl;
-    std::cout << "Part 2: " << r1d1 * r3d1 * r5d1 * r7d1 * r1d2 << std::endl;
+        for (Accumulator& acc : ratios)
+            acc.check(row);
+  
+    std::cout << "Part 1: " << (long long)ratios.front() << std::endl;
+    std::cout << "Part 2: " << std::accumulate(ratios.cbegin(), ratios.cend(), 1ll, std::multiplies()) << std::endl;
+        
     return 0;
 }
---
+++ Day03F.fs
@@ -0,0 +1,21 @@
+﻿open System.IO
+
+let CheckLine (x, y, dx, dy, count) (line:string) =
+    let nCount = match y = 0 && line.[x] = '#' with | true -> count+1L | _ -> count
+    let ny = match y+1 = dy with | true -> 0 | _ -> y + 1
+    let nx = match ny = 0 with | false -> x | _ -> (x+dx) % (line.Length-1)
+    (nx, ny, dx, dy, nCount)
+
+let GetCount (_, _, _, _, count) = count
+
+let ProcessSlope slope lines =
+   Array.fold(fun state line -> CheckLine state line) slope lines |> GetCount
+
+[<EntryPoint>]
+let main argv =
+    let data = File.ReadAllText(argv.[0]).Split('\n')
+    let slopes = [| (0, 0, 3, 1, 0L); (0, 0, 1, 1, 0L); (0, 0, 5, 1, 0L); (0, 0, 7, 1, 0L); (0, 0, 1, 2, 0L)|]
+    let results = Array.map(fun x -> ProcessSlope x data) slopes
+    printfn "Part 1: %A" results.[0]
+    Array.reduce(*) results |> printfn "Part 2: %A" 
+    0 // return an integer exit code
