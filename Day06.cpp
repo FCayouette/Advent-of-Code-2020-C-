6bbc06ed92dd752d7435a435ecc6f7542fafbe7d
Francois Cayouette <45644908+FCayouette@users.noreply.github.com>
2020-12-06 00:22:05 -0500

    Day 6 C++ solution and data

---
+++ Day06.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <array>
+
+int main(int argc, char* argv[])
+{
+	if (argc < 2)
+	{
+		std::cout << "Usage: Day05.exe DataFilename" << std::endl;
+		return -1;
+	}
+
+	std::ifstream in(argv[1], std::ios::in);
+	if (!in) return -1;
+
+	std::array<char, 30> line;
+	std::array<int, 26> histogram;
+	for (int i = 0; i < 26; ++i)
+		histogram[i] = 0;
+	int part1 = 0, part2 = 0, groupCount = 0;
+	
+	auto CountGroup = [&part1, &part2, &groupCount, &histogram]()
+	{
+		for (int i : histogram)
+		{
+			if (i != 0)
+				++part1;
+			if (i == groupCount)
+				++part2;
+		}
+	};
+
+	while (in.getline(&line[0], 30))
+	{
+		if (strlen(&line[0]) == 0)
+		{
+			CountGroup();
+			for (int i = 0; i < 26; ++i)
+				histogram[i] = 0;
+			groupCount = 0;
+		}
+		else
+		{
+			int i = 0;
+			while (line[i] != '\0')
+				++histogram[line[i++] - 'a'];
+			++groupCount;
+		}
+	}
+	CountGroup();
+	
+	std::cout << "Part 1: " << part1 << std::endl;
+	std::cout << "Part 2: " << part2 << std::endl;
+	return 0;
+}
