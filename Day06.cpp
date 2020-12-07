854821ee399c98495b8e74077f4cc31df09b8687
Francois Cayouette <45644908+FCayouette@users.noreply.github.com>
2020-12-06 20:28:39 -0500

    Day 6: F# Solution and improved C++ solution

--- Day06.cpp
+++ Day06.cpp
@@ -7,17 +7,15 @@ int main(int argc, char* argv[])
 {
 	if (argc < 2)
 	{
-		std::cout << "Usage: Day05.exe DataFilename" << std::endl;
+		std::cout << "Usage: Day06.exe DataFilename" << std::endl;
 		return -1;
 	}
 
 	std::ifstream in(argv[1], std::ios::in);
 	if (!in) return -1;
 
-	std::array<char, 30> line;
 	std::array<int, 26> histogram;
-	for (int i = 0; i < 26; ++i)
-		histogram[i] = 0;
+	histogram.fill(0);
 	int part1 = 0, part2 = 0, groupCount = 0;
 	
 	auto CountGroup = [&part1, &part2, &groupCount, &histogram]()
@@ -31,13 +29,13 @@ int main(int argc, char* argv[])
 		}
 	};
 
+	std::array<char, 30> line;
 	while (in.getline(&line[0], 30))
 	{
 		if (strlen(&line[0]) == 0)
 		{
 			CountGroup();
-			for (int i = 0; i < 26; ++i)
-				histogram[i] = 0;
+			histogram.fill(0);
 			groupCount = 0;
 		}
 		else
---
+++ Day06F.fs
@@ -0,0 +1,15 @@
+﻿open System.IO
+
+let Consolidate (consolidated, (acc:string), count) (raw:string) =
+    let filtered = String.filter(fun c -> c <> '\r') raw
+    if filtered.Length = 0 then
+        (Array.append consolidated [|(acc.ToCharArray(), count)|], "", 0)
+    else (consolidated, acc + filtered, count + 1)
+
+[<EntryPoint>]
+let main argv =
+    let (t1, t2, t3) = Array.fold(Consolidate) ([||], "", 0) <| File.ReadAllText(argv.[0]).Split('\n')
+    let groups = Array.append(t1) [| (t2.ToCharArray(), t3) |]
+    printfn "Part 1: %A" (Array.fold(fun total (x,_) -> (x |> Array.distinct).Length + total) 0 groups)
+    printfn "Part 2: %A" (Array.fold(fun total (x, count) -> (x |> Array.countBy(id) |> Array.filter(fun (_,x) -> x=count)).Length + total) 0 groups)
+    0 // return an integer exit code
