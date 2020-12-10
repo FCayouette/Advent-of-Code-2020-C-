06ff166e087054d19ded6481878b38b03f12e996
Francois Cayouette <45644908+FCayouette@users.noreply.github.com>
2020-12-09 21:54:50 -0500

    Day 9: F# and improved C++ solution

--- Day09.cpp
+++ Day09.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <fstream>
-#include <deque>
 #include <vector>
 #include <algorithm>
 
@@ -10,20 +9,18 @@ int main(int argc, char* argv[])
 {
 	if (argc < 2)
 	{
-		std::cout << "Usage: Day07.exe DataFilename" << std::endl;
+		std::cout << "Usage: Day09.exe DataFilename" << std::endl;
 		return -1;
 	}
 
 	std::ifstream in(argv[1], std::ios::in);
 	if (!in) return -1;
 
-	std::deque<u64> nums;
 	std::vector<u64> values;
 	u64 num = 0;
 	for (int i = 0; i < 25; ++i)
 	{
 		in >> num;
-		nums.push_back(num);
 		values.push_back(num);
 	}
 
@@ -31,42 +28,33 @@ int main(int argc, char* argv[])
 
 	while (in >> num)
 	{
-		bool found = false;
-		for (size_t i = 0; i < nums.size() && !found; ++i)
-			for (size_t j = i + 1; j < nums.size(); ++j)
-				if (nums[i] + nums[j] == num)
-				{
-					found = true;
-					break;
-				}
-
-		if (!found && !part1)
-			part1 = num;
-		
 		if (!part1)
 		{
-			nums.pop_front();
-			nums.push_back(num);
+			bool found = false;
+			for (size_t i = values.size() - 25; i < values.size() && !found; ++i)
+				for (size_t j = i + 1; j < values.size(); ++j)
+					if (values[i] + values[j] == num)
+					{
+						found = true;
+						break;
+					}
+
+			if (!found)
+				part1 = num;
 		}
 		values.push_back(num);
 	}
 	std::cout << "Part 1: " << part1 << std::endl;
-	bool found = false;
-	for (size_t i = 0; i < values.size() && !found; ++i)
+
+	u64 runningSum = values[0];
+	size_t low = 0, high = 1;
+	while (runningSum != part1)
 	{
-		u64 runningSum = 0ull;
-		for (size_t j = i; j < values.size() && runningSum < part1; ++j)
-		{
-			runningSum += values[j];
-			if (runningSum == part1)
-			{
-				std::sort(values.begin() + i, values.begin() + j + 1);
-				std::cout << "Part 2: " << values[i] + values[j] << std::endl;
-				found = true;
-				break;
-			}
-		}
+		while (runningSum < part1) runningSum += values[high++];
+		while (runningSum > part1) runningSum -= values[low++];
 	}
 
+	std::sort(values.begin() + low, values.begin() + high);
+	std::cout << "Part 2: " << values[low] + values[high-1] << std::endl;
 	return 0;
 }
---
+++ Day09.fs
@@ -0,0 +1,24 @@
+﻿open System.IO
+
+let FindOddOne line (values:(int*int64)[]) =
+    if line < 25 then false
+    else let work = values.[line-25..line-1] |> Array.map(snd)
+         (seq { for i in 0 .. 23 do
+                for j in i+1 ..24 do work.[i] + work.[j] } |> Seq.filter(fun x -> x = (snd values.[line])) |> Seq.toArray).Length = 0
+
+let rec FindMinMaxSequence (l, h, running) target (values:_[]) =
+    if running = target then
+        let s = values.[l..h]
+        Array.min s + Array.max s
+    else if running < target then 
+        FindMinMaxSequence (l, h+1, running + values.[h]) target values
+        else FindMinMaxSequence (l+1, h, running - values.[l]) target values
+
+[<EntryPoint>]
+let main argv =
+    let values = File.ReadAllLines(argv.[0]) |> Array.map System.Int64.Parse
+    let indexedValue = Array.indexed values
+    let part1 = snd (Array.filter(fun (i, x) -> FindOddOne i indexedValue) indexedValue).[0]
+    printfn "Part 1: %A" part1
+    printfn "Part 2: %A" <| FindMinMaxSequence (0,0,0L) part1 values
+    0 // return an integer exit code
