867d01edc46e53f7b02d1e7ed72d02b21e92888a
Francois Cayouette <45644908+FCayouette@users.noreply.github.com>
2020-12-10 23:09:33 -0500

    Day 10: F# and improved C++ solution

--- Day10.cpp
+++ Day10.cpp
@@ -17,48 +17,35 @@ int main(int argc, char* argv[])
 
     std::string value;
     std::vector<int> values{ 0 };
-    std::vector<char> locked{ 1 };
 
     while (in >> value)
-    {
         values.push_back(stoi(value));
-        locked.push_back(0);
-    }
-
+    
     std::sort(values.begin(), values.end());
     values.push_back(values.back() + 3);
-    locked.push_back(1);
-    size_t ones = 0, threes = 0;
+    size_t ones = 0, threes = 0, count = 0;
+    unsigned long long part2 = 1ull;
+    
     for (size_t i = 1; i < values.size(); ++i)
     {
-        if (values[i - 1] + 1 == values[i])
+        if (int diff = values[i] - values[i - 1];
+            diff == 1)
+        {
             ++ones;
+            ++count;
+        }
         else
         {
             ++threes;
-            locked[i - 1] = locked[i] = 1;
+            if (count > 0)
+            {
+                --count;
+                part2 *= (count < 2 ? count == 1 ? 2 : 1 : count == 2 ? 4 : 7);
+                count = 0;
+            }
         }
     }
 
-    std::cout << "Part 1: " << ones * threes << std::endl;
-    unsigned long long part2 = 1ull;
-    
-    size_t iter = 1;
-    while (iter != locked.size())
-    {
-        if (locked[iter])
-            ++iter;
-        else
-        {
-            int count = 1;
-            while (!locked[++iter])
-                ++count;
-            if (count == 1) part2 *= 2ull;
-            else if (count == 2) part2 *= 4ull;
-            else if (count == 3) part2 *= 7ull;
-        }
-    }
-
-    std::cout << "Part 2: " << part2 << std::endl;
+    std::cout << "Part 1: " << ones * threes << std::endl << "Part 2: " << part2 << std::endl;
     return 0;
 }
---
+++ Day10F.fs
@@ -0,0 +1,20 @@
+﻿open System.IO
+
+let Part2 (acc, count) (l,h) =
+    if (h-l) < 3 then (acc, count+1)
+    else match count-1 with 
+                          | 1 -> (acc*2L, 0)
+                          | 2 -> (acc*4L, 0)
+                          | 3 -> (acc*7L, 0)
+                          | _ -> (acc, 0)
+
+[<EntryPoint>]
+let main argv =
+    let partial = Array.append [|0|] (File.ReadAllLines(argv.[0]) |> Array.map System.Int32.Parse |> Array.sort)
+    let adaptors = Array.append partial [|3 + Array.last partial|]
+    let pairedAdaptors = Array.pairwise adaptors
+    let part1 = Array.fold(fun (ones, threes) (f,s) -> match s-f with |1 ->(ones+1, threes) | 3 -> (ones, threes+1) |_->(ones,threes)) (0,0) pairedAdaptors
+    
+    printfn "Part 1: %A" (fst part1 * snd part1)
+    printfn "Part 2: %A" (fst (Array.fold(Part2) (1L, 0) pairedAdaptors))
+    0 // return an integer exit code
