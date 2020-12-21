ba1bae98cf9021caf09cd49a1da91731b93c9b5f
Francois Cayouette <45644908+FCayouette@users.noreply.github.com>
2020-12-21 15:10:45 -0500

    Day 21: Improved performance C++ solution

--- Day21.cpp
+++ Day21.cpp
@@ -3,7 +3,9 @@
 #include <string>
 #include <vector>
 #include <map>
+#include <unordered_map>
 #include <algorithm>
+#include <numeric>
 
 int main(int argc, char* argv[])
 {
@@ -16,12 +18,34 @@ int main(int argc, char* argv[])
     std::ifstream in(argv[1], std::ios::in);
     if (!in) return -1;
 
-    std::map<std::string, std::vector<std::string>> allPos;
-    std::map<std::string, size_t> ingCounts;
-    std::string data, part2;
+    std::unordered_map<std::string, size_t> ingredientNumber, allergenNumber;
+    std::vector<std::string> numberToIngredient, numberToAllergen;
+
+    auto GetIngredientNumber = [&ingredientNumber, &numberToIngredient](const std::string& ing) -> size_t
+    {
+        if (auto iter = ingredientNumber.find(ing);
+            iter != ingredientNumber.cend())
+            return iter->second;
+        ingredientNumber[ing] = numberToIngredient.size();
+        numberToIngredient.push_back(ing);
+        return numberToIngredient.size() - 1;
+    };
+
+    auto GetAllergenNumber = [&allergenNumber, &numberToAllergen](const std::string& all) -> size_t
+    {
+        if (auto iter = allergenNumber.find(all);
+            iter != allergenNumber.cend())
+            return iter->second;
+        allergenNumber[all] = allergenNumber.size();
+        numberToAllergen.push_back(all);
+        return numberToAllergen.size() - 1;
+    };
+
+    std::map<size_t, std::vector<size_t>> allergenPossibilities;
+    std::vector<size_t> ingredientCounts, lineIngredients, lineAllergens;
+    std::string data;
     bool allergens = false;
 
-    std::vector<std::string> lineIng, lineAllergens;
     size_t part1 = 0;
     while (in >> data)
     {
@@ -29,56 +53,60 @@ int main(int argc, char* argv[])
             allergens = true;
         else if (!allergens)
         {
-            lineIng.push_back(data);
-            ++ingCounts[data];
             ++part1;
+
+            size_t iNum = GetIngredientNumber(data);
+            lineIngredients.push_back(iNum);
+            if (iNum == ingredientCounts.size())
+                ingredientCounts.push_back(1);
+            else
+                ++ingredientCounts[iNum];
         }
         else
         {
-            lineAllergens.push_back(data.substr(0, data.size() - 1));
+            lineAllergens.push_back(GetAllergenNumber(data.substr(0, data.size() - 1)));
+
             allergens = data[data.size() - 1] == ',';
             if (!allergens)
             {
-                std::sort(lineIng.begin(), lineIng.end());
-                for (const std::string& a : lineAllergens)
+                std::sort(lineIngredients.begin(), lineIngredients.end());
+                for (size_t a : lineAllergens)
                 {
-                    if (allPos.find(a) == allPos.cend())
-                        allPos[a] = lineIng;
+                    if (auto iter = allergenPossibilities.find(a);
+                        iter == allergenPossibilities.cend())
+                        allergenPossibilities[a] = lineIngredients;
                     else
                     {
-                        std::vector<std::string>& curPos = allPos[a];
-                        std::vector<std::string> newPos;
-                        std::set_intersection(lineIng.cbegin(), lineIng.cend(), curPos.cbegin(), curPos.cend(), std::back_inserter(newPos));
-                        allPos[a] = newPos;
+                        std::vector<size_t>& currentPos = iter->second, newPos;
+                        std::set_intersection(lineIngredients.cbegin(), lineIngredients.cend(), currentPos.cbegin(), currentPos.cend(), std::back_inserter(newPos));
+                        std::swap(newPos, currentPos);
                     }
                 }
-                lineIng.clear();
+                lineIngredients.clear();
                 lineAllergens.clear();
             }
         }
     }
-    std::vector<std::pair<std::string, std::string>> matches;
+    std::vector<std::pair<std::string, size_t>> allToIng;
 
-    while (allPos.size())
+    while (allergenPossibilities.size())
     {
-        std::map<std::string, std::vector<std::string>>::iterator iter = allPos.begin();
+        std::map<std::size_t, std::vector<size_t>>::iterator iter = allergenPossibilities.begin();
         while (iter->second.size() != 1)
             ++iter;
-        std::string toDelete = iter->second.front();
-        part1 -= ingCounts[toDelete];
-        matches.emplace_back(iter->first, toDelete);
-        allPos.erase(iter);
-        for (auto& x : allPos)
+        size_t toDelete = iter->second.front();
+        part1 -= ingredientCounts[toDelete];
+        allToIng.emplace_back(numberToAllergen[iter->first], toDelete);
+        allergenPossibilities.erase(iter);
+        for (auto& x : allergenPossibilities)
             if (auto extra = std::find(x.second.begin(), x.second.end(), toDelete);
                 extra != x.second.end())
                 x.second.erase(extra);
-    }    
-    std::cout << "Part 1: " << part1 << std::endl;
+    }
 
-    std::sort(matches.begin(), matches.end());
-    for (size_t i = 0; i < matches.size() - 1; ++i)
-        part2 += matches[i].second + ',';
-    part2 += matches.back().second;
-    std::cout << part2 << std::endl;
+    std::sort(allToIng.begin(), allToIng.end());
+    std::cout << "Part 1: " << part1 << "\nPart 2: " 
+        << std::accumulate(std::next(allToIng.cbegin()), allToIng.cend(), numberToIngredient[allToIng[0].second], 
+                           [&numberToIngredient](const std::string& l, const auto& p) {return l + ',' + numberToIngredient[p.second]; }) << std::endl;
     return 0;
 }
