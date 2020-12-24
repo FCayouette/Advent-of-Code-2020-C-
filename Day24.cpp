5f6480bcf079858ef541f9f94de1b49b51f85367
Francois Cayouette <45644908+FCayouette@users.noreply.github.com>
2020-12-24 01:44:52 -0500

    Day 24: Better C++ solution

--- Day24.cpp
+++ Day24.cpp
@@ -17,15 +17,15 @@ int main(int argc, char* argv[])
     }
     std::ifstream in(argv[1], std::ios::in);
     if (!in) return -1;
-    std::array<Point3D, 6> deltas{ Point3D(-1,-1,0), Point3D(-1,0,1), Point3D(0,1,1), Point3D(1,1,0), Point3D(1,0,-1), Point3D(0, -1, -1) };
+    std::array<Point, 6> deltas{ Point(-1,0), Point(0,-1), Point(1,-1), Point(1, 0), Point(0, 1), Point(-1,1) };//e ne nw w sw se
 
-    std::map<Point3D, size_t> floor;
+    std::map<Point, size_t> floor;
 
     std::string line;
     while (in >> line)
     {
         size_t tokenLength = 0;
-        Point3D tile(0, 0, 0);
+        Point tile(0, 0);
         do
         {
             if (char c = line[0];
@@ -49,32 +49,32 @@ int main(int argc, char* argv[])
         ++floor[tile];
     }
 
-    std::set<Point3D> blackTiles, work;
+    std::set<Point> blackTiles, work;
     for (const auto& t : floor)
         if (t.second % 2)
             blackTiles.insert(t.first);
     std::cout << "Part 1: " << blackTiles.size() << std::endl;
 
-    std::vector<Point3D> adjacencies;
+    std::vector<Point> adjacencies;
 
     for (size_t turns = 0; turns < 100; ++turns)
     {
         adjacencies.reserve(6 * blackTiles.size());
-        for (const Point3D& p : blackTiles)
-            std::for_each(deltas.cbegin(), deltas.cend(), [&adjacencies, &p](const Point3D& d) { adjacencies.push_back(p + d); });
+        for (const Point& p : blackTiles)
+            std::for_each(deltas.cbegin(), deltas.cend(), [&adjacencies, &p](const Point& d) { adjacencies.push_back(p + d); });
 
         std::sort(adjacencies.begin(), adjacencies.end());
 
-        for (const Point3D& p : blackTiles)
+        for (const Point& p : blackTiles)
             if (auto iter = std::find(adjacencies.cbegin(), adjacencies.cend(), p);
                 iter != adjacencies.cend())
-                if (auto iter2 = std::find_if(iter, adjacencies.cend(), [&p](const Point3D a) { return a != p; }); 
+                if (auto iter2 = std::find_if(iter, adjacencies.cend(), [&p](const Point a) { return a != p; }); 
                     std::distance(iter, iter2) < 3)
                     work.insert(p);
             
         for (auto iter = adjacencies.cbegin(); iter != adjacencies.cend();)
         {
-            auto iter2 = std::find_if(iter, adjacencies.cend(), [p3d = *iter](const Point3D& p) { return p != p3d; }); 
+            auto iter2 = std::find_if(iter, adjacencies.cend(), [p3d = *iter](const Point& p) { return p != p3d; }); 
             if (std::distance(iter, iter2) == 2 && blackTiles.find(*iter) == blackTiles.cend())
                 work.insert(*iter);
             iter = iter2;
