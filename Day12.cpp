fabacc1eefa5d3a803e86673a3c70ef20867d5b8
Francois Cayouette <45644908+FCayouette@users.noreply.github.com>
2020-12-30 14:30:59 -0500

    Day 12: F# solution and improved C++ solution

--- Day12.cpp
+++ Day12.cpp
@@ -14,51 +14,41 @@ int main(int argc, char* argv[])
     std::ifstream in(argv[1], std::ios::in);
     if (!in) return -1;
 
-    Point position, origin;
+    Point part1, part2, origin, waypoint(1, 10);
     char dir = 1; // East
-    std::vector<std::pair<char, int>> instructions;
 
     std::string instruction;
     while (in >> instruction)
     {
         char c = instruction[0];
         int val = stoi(instruction.substr(1));
-        instructions.emplace_back(c, val);
         switch (c)
         {
         case 'L':
-            do dir = dir == 0 ? 3 : dir - 1;
-            while ((val -= 90) > 0);
+            do {
+                dir = (dir + 3) % coords.size();
+                waypoint = Point(waypoint.y, -waypoint.x);
+            } while ((val -= 90) > 0);
             break;
         case 'R':
-            do dir = (dir + 1) % coords.size();
-            while ((val -= 90) > 0);
+            do {
+                dir = (dir + 1) % coords.size();
+                waypoint = Point(-waypoint.y, waypoint.x);
+            } while ((val -= 90) > 0);
             break;
-        default: position += directions[(c != 'F') ? std::distance(coords.cbegin(), std::find(coords.cbegin(), coords.cend(), c)) : dir] * val;
+        case 'F':
+            part1 += directions[dir] * val;
+            part2 += waypoint * val;
+            break;
+        default:
+        {
+            Point delta = directions[std::distance(coords.cbegin(), std::find(coords.cbegin(), coords.cend(), c))] * val;
+            part1 += delta;
+            waypoint += delta;
+        }
         }
     }
 
-    std::cout << "Part 1: " << ManhattanDistance(position, origin) << std::endl;
-
-    position = origin;
-    Point waypoint(1, 10);
-
-    for (std::pair<char, int> inst : instructions)
-        switch (inst.first)
-        {
-        case 'L':
-            do waypoint = Point(waypoint.y, -waypoint.x);
-            while ((inst.second -= 90) > 0);
-            break;
-        case 'R':
-            do waypoint = Point(-waypoint.y, waypoint.x);
-            while ((inst.second -= 90) > 0);
-            break;
-        case 'F': position += waypoint * inst.second; break;
-        default: waypoint += directions[std::distance(coords.cbegin(), std::find(coords.cbegin(), coords.cend(), inst.first))] * inst.second;
-        }
-
-    std::cout << "Part 2: " << ManhattanDistance(position, origin) << std::endl;
-
+    std::cout << "Part 1: " << ManhattanDistance(part1, origin) << std::endl << "Part 2: " << ManhattanDistance(part2, origin) << std::endl;
     return 0;
 }
---
+++ Day12F.fs
@@ -0,0 +1,36 @@
+﻿open System.IO
+
+let Manhattan(x, y, _) = abs(x) + abs(y)
+
+let rec NewDir d (c, deg) =
+    let nd = match c with | 'L' -> (d+3)%4 | _ -> (d+1)%4
+    match deg > 90 with | true -> NewDir nd (c, deg-90) | _-> nd
+
+let directions = [|(-1,0); (0,1); (1,0); (0,-1)|]
+
+let Part1 (x, y, dir) (d, n) =
+    match d with | 'F' -> (x+ fst directions.[dir] * n, y + snd directions.[dir] * n, dir)
+                 | 'E' -> (x + fst directions.[0] * n, y, dir)
+                 | 'S' -> (x, y + snd directions.[1] * n, dir)
+                 | 'W' -> (x + fst directions.[2] * n, y, dir)
+                 | 'N' -> (x, y + snd directions.[3] * n, dir)
+                 | _ -> (x, y, NewDir dir (d, n))
+
+let rec RotateWaypoint (wx, wy) (c, deg) =
+    let nw = match c with | 'L' -> (-wy, wx) | _ -> (wy,-wx)
+    match deg > 90 with | true -> RotateWaypoint nw (c, deg-90) | _ -> nw
+
+let Part2 (x, y, (wx, wy)) (d,n) =
+    match d with | 'F' -> (x + wx*n, y + wy*n, (wx,wy))
+                 | 'E' -> (x, y, (wx + fst directions.[0] * n, wy))
+                 | 'S' -> (x, y, (wx, wy + snd directions.[1] * n))
+                 | 'W' -> (x, y, (wx + fst directions.[2] * n, wy))
+                 | 'N' -> (x, y, (wx, wy + snd directions.[3] * n))
+                 | _ -> (x, y, RotateWaypoint (wx, wy) (d,n))
+
+[<EntryPoint>]
+let main argv =
+    let directions = File.ReadAllLines(argv.[0]) |> Array.map(fun (s:string) -> (s.[0], s.[1..] |> System.Int32.Parse))
+    Manhattan (Array.fold(Part1) (0,0,0) directions) |> printfn "Part 1: %A" 
+    Manhattan (Array.fold(Part2) (0,0,(-10,-1)) directions) |> printfn "Part 2: %A"
+    0 // return an integer exit code
