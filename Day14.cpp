5175b6b88f12842fd22a3f92be2d8039f7623e45
Francois Cayouette <45644908+FCayouette@users.noreply.github.com>
2021-01-03 23:08:45 -0500

    Day 14: F# solution and optimized C++ solution

--- Day14.cpp
+++ Day14.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-#include <map>
 #include <numeric>
+#include <unordered_map>
 #include "TemplatedUtilities.h"
 
 using u64 = unsigned long long;
@@ -18,23 +18,23 @@ int main(int argc, char* argv[])
     std::ifstream in(argv[1], std::ios::in);
     if (!in) return -1;
 
-    std::map<u64, u64> memory1, memory2;
-    std::string start, equal, val, mask;
+    std::unordered_map<u64, u64> memory1, memory2;
+    std::string start, equal, val, mask, part2Mask;
 
-    auto WriteAddresses = y_combinator([&memory2](auto&& WriteAddresses, u64 val, const std::string& mask, size_t where) {
-        while (where < mask.size() && mask[where] != 'X')
+    auto WriteAddresses = y_combinator([&memory2, &part2Mask](auto&& WriteAddresses, u64 val, size_t where) {
+        while (where < part2Mask.size() && part2Mask[where] != 'X')
             ++where;
-        if (where >= mask.size())
+        if (where >= part2Mask.size())
         {
-            u64 address = std::stoll(mask, nullptr, 2);
+            u64 address = std::stoll(part2Mask, nullptr, 2);
             memory2[address] = val;
             return;
         }
-        std::string nMask = mask;
-        nMask[where] = '0';
-        WriteAddresses(val, nMask, where + 1);
-        nMask[where++] = '1';
-        WriteAddresses(val, nMask, where);
+        part2Mask[where] = '0';
+        WriteAddresses(val, where + 1);
+        part2Mask[where] = '1';
+        WriteAddresses(val, where);
+        part2Mask[where] = 'X';
         });
 
     while (in >> start >> equal >> val)
@@ -56,16 +56,16 @@ int main(int argc, char* argv[])
             memory1[index] = value;
 
             // part 2
-            std::string newMask = mask;
+            part2Mask = mask;
             currentBit = 1ull;
-            for (auto iter = newMask.rbegin(); iter != newMask.rend(); ++iter, currentBit*=2ull)
+            for (auto iter = part2Mask.rbegin(); iter != part2Mask.rend(); ++iter, currentBit*=2ull)
                 if (*iter == '0' && index & currentBit)
                         *iter = '1';
-            WriteAddresses(part2Val, newMask, 0);
+            WriteAddresses(part2Val, 0);
         }
     }
 
-    std::cout << "Part 1:" << std::accumulate(memory1.cbegin(), memory1.cend(), 0ull, [](u64 l, const auto& p) { return l + p.second; }) << std::endl;
+    std::cout << "Part 1: " << std::accumulate(memory1.cbegin(), memory1.cend(), 0ull, [](u64 l, const auto& p) { return l + p.second; }) << std::endl;
     std::cout << "Part 2: " << std::accumulate(memory2.cbegin(), memory2.cend(), 0ull, [](u64 l, const auto& p) { return l + p.second; }) << std::endl;
     return 0;
 }
---
+++ Day14F.fs
@@ -0,0 +1,43 @@
+﻿open System.IO
+
+let FormatData (t, (mask:string), mem) (s:string) =
+    if s.[1] = 'a' then
+        if mask.Length > 0 then
+            (Array.append t [|(mask, mem)|], s.[7..], [])
+        else (t, s.[7..], [])
+    else
+        let v = s.Replace("mem[", "").Replace("] = ", " ").Split(' ') |> Array.map System.UInt64.Parse
+        (t, mask, mem @ [(v.[0], v.[1])])
+
+let rec GetModifiedValue (mask:string) power i value =
+    if i > mask.Length then value
+    else let newPower = power*2UL
+         let nextI = i + 1
+         match mask.[mask.Length - i] with | '1' -> GetModifiedValue mask newPower nextI value ||| power
+                                           | '0' -> GetModifiedValue mask newPower nextI value &&& ~~~power
+                                           | _ -> GetModifiedValue mask newPower nextI value
+
+let Part1 (m:Map<_,_>) (mask:string, (mem:((uint64*uint64)list))) =
+    mem |> List.fold(fun (a:Map<_,_>) (address, value) -> Map.add address (GetModifiedValue mask 1UL 1 value) a) m
+
+let rec WriteToModifiedAddress (m:Map<_,_>) address (mask:string) value power i =
+    if i > mask.Length then Map.add address value m
+    else let newPower = power*2UL
+         let nextI = i + 1
+         match mask.[mask.Length - i] with | '1' -> WriteToModifiedAddress m (address ||| power) mask value newPower nextI
+                                           | '0' -> WriteToModifiedAddress m address mask value newPower nextI
+                                           | _ -> let tmp = WriteToModifiedAddress m (address ||| power) mask value newPower nextI
+                                                  WriteToModifiedAddress tmp (address &&& ~~~ power) mask value newPower nextI
+
+let Part2 (m:Map<_,_>) (mask:string, (mem:((uint64*uint64)list))) =
+    mem |> List.fold(fun (a:Map<_,_>) (address, value) -> WriteToModifiedAddress a address mask value 1UL 1) m
+
+let AddValuesInMemory m = Map.fold(fun total _ v -> total + v) 0UL m
+
+[<EntryPoint>]
+let main argv =
+    let (tmpData, lastMask, lastMem) = File.ReadAllLines(argv.[0]) |> Array.fold(FormatData) ([||], "", [])
+    let data = Array.append tmpData [|(lastMask, lastMem)|]
+    Array.fold(Part1) Map.empty data |> AddValuesInMemory |> printfn "Part 1: %A"
+    Array.fold(Part2) Map.empty data |> AddValuesInMemory |> printfn "Part 2: %A"
+    0
